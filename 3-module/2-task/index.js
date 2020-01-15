05192bd6e44cd1fc9bbf1d658b46a949b62d805f
Aleksandr Menshchikov <aleksandr.menshchikov1982@gmail.com>
2020-01-15 22:25:40 +0300

    update

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -3,5 +3,12 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
-function getMinMax(str) {
+function getMinMax(string) {
+  let arr = string.match(/([-+]?(\d+)[\.]?(\d+)?)/gm);
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let total = {min, max};
+    
+  return total;
 }
+
