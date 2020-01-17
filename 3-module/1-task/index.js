3d7d7652ba5b9f542b0ccadb86cf5b1cde8e9ef5
AleksandrMenshchikov <51015585+AleksandrMenshchikov@users.noreply.github.com>
2020-01-17 23:32:42 +0300

    Update

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,4 +5,11 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+  let str = '';
+  for (let elem of data) {
+    if (elem.age <= age) {
+      str += `${elem.name}, ${elem.balance}\n`;
+    }
+  }
+  return str.split('\n').slice(0, -1).join('\n');
 }
