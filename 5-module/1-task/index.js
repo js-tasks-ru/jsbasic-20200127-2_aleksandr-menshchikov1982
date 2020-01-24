525b6fb2584ff5f5a613ec162e8107dd595f9703
Aleksandr Menshchikov <aleksandr.menshchikov1982@gmail.com>
2020-01-25 02:20:46 +0300

    created function highlight(table)

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -3,4 +3,23 @@
  * @param {Element} table
  */
 function highlight(table) {
+  for (let elem of table.children[1].children) {
+    if (elem.children[3].dataset.available == 'true') {
+      elem.classList.add('available');
+    } else if (elem.children[3].dataset.available == 'false') {
+      elem.classList.add('unavailable');
+    } else if (!elem.children[3].hasAttribute('data-available')) {
+      elem.hidden = true;
+    } 
+
+    if (elem.children[2].textContent == 'm') {
+      elem.classList.add('male');
+    } else if (elem.children[2].textContent == 'f') {
+      elem.classList.add('female');
+    }
+    
+    if (elem.children[1].textContent < 18) {
+      elem.style.textDecoration = 'line-through';
+    }
+  }
 }
